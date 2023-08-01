dfe88346ad376d89e0df5fabdab6439ba35ee8d3
Davi Coelho <davic2stl@outlook.com>
2023-07-31 22:45:42 -0300

    gitignore e pequenos incrementos para o fastapi

--- Backend/app.py
+++ Backend/app.py
@@ -1,23 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 from fastapi.middleware.cors import CORSMiddleware
 
 api = FastAPI()
 
+api.api_route = APIRouter(prefix='/api')
+
 api.api_route('')
 
-origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
-    "http://localhost",
-    "http://localhost:8080",
-]
 
 api.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-
