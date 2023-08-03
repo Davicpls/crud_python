1e867e38e418644c9199734b522087ec8843c24e
Davi Coelho <davic2stl@outlook.com>
2023-08-02 23:39:17 -0300

    modulando, testando e compreendendo uso das classes

---
+++ Backend/models/classes/items.py
@@ -0,0 +1,7 @@
+class Items:
+    def __init__(self, item_id: int, name: str, description: str, quantity: float, price: float):
+        self.id = item_id
+        self.name = name
+        self.description = description
+        self.quantity = quantity
+        self.price = price
---
+++ Backend/models/classes/user_items.py
@@ -0,0 +1,4 @@
+class UserItems:
+    def __init__(self, user_id: int, item_id: int):
+        self.user_id = user_id
+        self.item_id = item_id
